e7d38b0f9170ad4d1a45c8dd27f0539f83834327
annlozada <87684947+annlozada@users.noreply.github.com>
2021-08-03 14:01:47 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyCwOmnDA5KBybeb03ERpJciZazr9XpvpwY",
-    authDomain: "bdwp801.firebaseapp.com",
-    projectId: "bdwp801",
-    storageBucket: "bdwp801.appspot.com",
-    messagingSenderId: "101167042392",
-    appId: "1:101167042392:web:d7002191e9597ac076df2d",
-    measurementId: "G-3520RE961G"
+  apiKey: "AIzaSyChJLNREsr1Ad4BaLvGuqJY-iaLX5ls3q0",
+  authDomain: "loggin-13bbc.firebaseapp.com",
+  projectId: "loggin-13bbc",
+  storageBucket: "loggin-13bbc.appspot.com",
+  messagingSenderId: "1091837225170",
+  appId: "1:1091837225170:web:2ac8f13cbce24fcddfe50b",
+  measurementId: "G-CESKWDKBG7"
 });
